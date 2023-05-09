64b733e26050b959ec42527b5c70ce22e5518b3a
Mertcncifter <Mertcncifter@gmail.com>
2023-05-09 20:15:00 +0300

    Initial Commit

---
+++ Realm-Example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Realm-Example
+//
+//  Created by mert can çifter on 9.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
